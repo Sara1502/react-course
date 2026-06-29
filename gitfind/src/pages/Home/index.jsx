42b6467b7780054f76a53c7767b6e0d3b74891a0
Sara1502 <sara.moreira1520@gmail.com>
2026-06-29 15:35:12 -0300

    Add barra e botão de pesquisa

--- gitfind/src/pages/Home/index.jsx
+++ gitfind/src/pages/Home/index.jsx
@@ -1,13 +1,20 @@
 import {Header} from '../../components/Header'
 import background from '../../assets/background.png'
 
+import './style.css';
+
 const App = () => {
     return (
         <div className="App">
             <Header />
             <div className='conteudo'>
                 <img src={background} className='background' />
-
+                <div className='info'>
+                    <div>
+                        <input name='usuario' placeholder='@username' />
+                        <button>Buscar</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
