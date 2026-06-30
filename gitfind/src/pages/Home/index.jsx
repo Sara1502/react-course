c6fb38db47c3663c61fcbbc23df993c455c6532c
Sara1502 <sara.moreira1520@gmail.com>
2026-06-30 09:31:00 -0300

    Add informações de perfil

--- gitfind/src/pages/Home/index.jsx
+++ gitfind/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import {Header} from '../../components/Header'
 import background from '../../assets/background.png'
+import profile from '../../assets/profile.png'
 
 import './style.css';
 
@@ -14,6 +15,15 @@ const App = () => {
                         <input name='usuario' placeholder='@username' />
                         <button>Buscar</button>
                     </div>
+                    <div className='perfil'>
+                        <img src={profile} className='profile' />
+                        <div className='descricao'>
+                            <h3>Sara Moreira</h3>
+                            <span>@sara</span>
+                            <p>Descrição</p>
+                        </div>
+                    </div>
+                    <hr />
                 </div>
             </div>
         </div>
